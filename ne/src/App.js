8465230dfd405f882df50aa88986ed32ae563a92
youmeanit <umin030312@naver.com>
2024-12-02 16:25:43 +0900

    카카오맵 제거

--- ne/src/App.js
+++ ne/src/App.js
@@ -1,6 +1,4 @@
-// src/App.js
-import React, { useState, useEffect } from 'react';
-import Map from './components/Kakaomap/Kakaomap';
+import React, { useState } from 'react';
 import MapFilters from './components/MapFilters/MapFilters';
 import './App.css';
 import { ReactComponent as Logo } from './components/images/logo.svg';
@@ -9,40 +7,26 @@ const App = () => {
   const [selectedCity, setSelectedCity] = useState('');
   const [selectedDistrict, setSelectedDistrict] = useState('');
   const [selectedDong, setSelectedDong] = useState('');
-  const [locations, setLocations] = useState([]);
 
-  // 서버에서 위치 데이터를 가져오기
-  useEffect(() => {
-    const fetchLocations = async () => {
-      try {
-        const response = await fetch(''); // API 엔드포인트
-        const data = await response.json();
-        setLocations(data.locations); // 서버로부터 받아온 데이터 형식에 따라 수정!!
-      } catch (error) {
-        console.error('Failed to fetch locations', error);
-      }
-    };
-
-    fetchLocations();
-  }, []); 
+  // 하드코딩된 위치 데이터
+  const locations = [
+    { id: 1, name: 'Location A', latitude: 37.5665, longitude: 126.9780 },
+    { id: 2, name: 'Location B', latitude: 37.5796, longitude: 126.9769 },
+    { id: 3, name: 'Location C', latitude: 37.5610, longitude: 126.9820 },
+  ];
 
   return (
     <div className="app">
       <header className="app-header">
-        <Logo/> 
+        <Logo />
         <MapFilters
-        onCityChange={(value) => setSelectedCity(value)}
-        onDistrictChange={(value) => setSelectedDistrict(value)}
-        onDongChange={(value) => setSelectedDong(value)}
-      />
+          onCityChange={(value) => setSelectedCity(value)}
+          onDistrictChange={(value) => setSelectedDistrict(value)}
+          onDongChange={(value) => setSelectedDong(value)}
+        />
       </header>
-     
-      <Map
-        city={selectedCity}
-        district={selectedDistrict}
-        dong={selectedDong}
-        locations={locations} // 서버에서 가져온 위치 데이터를 전달
-      />
+
+    
     </div>
   );
 };
